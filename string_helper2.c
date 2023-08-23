998690e6be5bc5109a1ff831b690c610ee414ae6
omar546 <omer.mkawy.om@gmail.com>
2023-08-23 15:12:03 +0200

    fix

--- string_helper2.c
+++ string_helper2.c
@@ -115,11 +115,11 @@ return (written);
  */
 int _strcmp_trim(const char *s1, const char *s2)
 {
+const char *end_s2 = s2 + _strlen(s2) - 1;
 while (*s1 && (*s1 == ' ' || *s1 == '\t' || *s1 == '\n'))
 {
 s1++;
 }
-const char *end_s2 = s2 + strlen(s2) - 1;
 while (end_s2 >= s2 && (*end_s2 == ' ' || *end_s2 == '\t' || *end_s2 == '\n'))
 {
 end_s2--;
